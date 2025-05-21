2e1b45f5e160e846a68e7a696c59cdcd1dcae71f
Giuliano Verrando <giulianoverrando@gmail.com>
2025-05-21 17:10:19 +0200

    prova hooks

--- src/freeports_analysis/filter_pdf.py
+++ src/freeports_analysis/filter_pdf.py
@@ -6,32 +6,32 @@ import pymupdf as pypdf
 
 parser = etree.XMLParser(recover=True)
 
+
 def EURIZON_filter(root: ET.Element) -> List[ET.Element]:
-    return root.findall('.//block')
+    return root.findall(".//block")
 
 
 def _DEFAULT_filter(root: ET.Element) -> List[ET.Element]:
     return [root]
 
 
-
 def relevant_text(format_pdf: fmt, document: pypdf.Document) -> str:
-    filter_func=None
+    filter_func = None
     match format_pdf:
         case fmt.EURIZON:
-            filter_func=EURIZON_filter
+            filter_func = EURIZON_filter
         case _:
-            filter_func=_DEFAULT_filter
-    text=''
+            filter_func = _DEFAULT_filter
+    text = ""
     for page in document:
-        xml_str=page.get_text("xml")
+        xml_str = page.get_text("xml")
         xml_tree = etree.fromstring(xml_str.encode(), parser=parser)
-        relevant_parts=filter_func(xml_tree)
+        relevant_parts = filter_func(xml_tree)
         for part in relevant_parts:
-            for line in part.findall('.//line'):
-                for e in line.findall('.//char'):
-                    c=e.get('c')
+            for line in part.findall(".//line"):
+                for e in line.findall(".//char"):
+                    c = e.get("c")
                     if c is not None:
-                        text+=c
-                text+='\n'
+                        text += c
+                text += "\n"
     return text
--- src/freeports_analysis/search.py
+++ src/freeports_analysis/search.py
@@ -3,23 +3,24 @@ from typing import List, Tuple, Optional
 from rapidfuzz import fuzz
 
 
-
-def _DEFAULT_search(row: str, targets: List[str]) -> Tuple[bool,Optional[Tuple[str,str]]]:
-    default_search_min_ratio=90
+def _DEFAULT_search(
+    row: str, targets: List[str]
+) -> Tuple[bool, Optional[Tuple[str, str]]]:
+    default_search_min_ratio = 90
     for target in targets:
-        if fuzz.partial_ratio(target.lower(),row.lower()) >= default_search_min_ratio:
+        if fuzz.partial_ratio(target.lower(), row.lower()) >= default_search_min_ratio:
             return (True, (target, row))
     return (False, None)
 
 
 def relevant_lines(pdf_format: fmt, text: str, targets: List[str]) -> List[str]:
-    search_func=None
+    search_func = None
     match pdf_format:
         case _:
-            search_func=_DEFAULT_search
-    relevant_rows=[]
+            search_func = _DEFAULT_search
+    relevant_rows = []
     for row in text.split("\n"):
-        matched, data= search_func(row,targets)
+        matched, data = search_func(row, targets)
         if matched:
             relevant_rows.append(data)
     print(relevant_rows[:100])
