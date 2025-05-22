1ffebe1538aef4d72b8bb8eca7ab44d69fc55692
Giuliano Verrando <giulianoverrando@gmail.com>
2025-05-22 09:30:05 +0200

    added download NOT EXISTANT URL test

--- src/freeports_analysis/download.py
+++ src/freeports_analysis/download.py
@@ -13,10 +13,9 @@ def download_pdf(url: str, pdf: str, save_pdf: bool):
     except Exception as e:
         logger.critical(e)
         raise e
-    finally:
-        file = BytesIO(response.content)
-        if save_pdf:
-            with open(pdf, "wb") as f:
-                f.write(file.read())
-            logger.debug("File %s saved on disk [in %s]", pdf, os.getcwd())
-        return file
+    file = BytesIO(response.content)
+    if save_pdf:
+        with open(pdf, "wb") as f:
+            f.write(file.read())
+        logger.debug("File %s saved on disk [in %s]", pdf, os.getcwd())
+    return file
---
+++ tests/test_download.py
@@ -0,0 +1,9 @@
+import conftest
+import pytest
+from freeports_analysis import download as dw
+from requests import ConnectionError
+
+
+def test_download_pdf_URL_NOT_FOUND():
+    with pytest.raises(ConnectionError):
+        dw.download_pdf("https://www.lkdjafdad.dkfljsa.org/documents/","report.pdf",False)
