6a1062bcc4774937c3523b910461cd92a7cd0480
Giuliano Verrando <giulianoverrando@gmail.com>
2025-06-02 01:26:53 +0200

    fixed EURIZON

--- src/freeports_analysis/formats/EURIZON/__init__.py
+++ src/freeports_analysis/formats/EURIZON/__init__.py
@@ -5,76 +5,117 @@ import re
 from rapidfuzz import fuzz
 from .. import PDF_Block, Text_Block
 import logging as log
+import datetime as dt
 
 logger = log.getLogger(__name__)
 
 
 def pdf_filter(xml_root: etree.Element) -> List[PDF_Block]:
-    parts = []
-    for blk in xml_root.findall(".//block"):
-        parts.append(PDF_Block(PDF_BlockType.TABLE, {}, blk))
+    parts=[]
+    portfolio_header=xml_root.xpath(".//line[contains(@text,'PORTFOLIO AS AT') and font[@name='Frutiger-Black']]")
+    if len(portfolio_header)>0:
+        table_lines=xml_root.xpath(".//line[font[@name='Frutiger-Light']]")
+        parts+=[PDF_Block(PDF_BlockType.LINES_TABLE,{},line) for line in table_lines]
     return parts
 
 
 def text_extract(pdf_blocks: List[PDF_Block], targets: List[str]) -> List[Text_Block]:
-    default_search_min_ratio = 90
-    text_part_list = []
-    for pdf_p in pdf_blocks:
-        row = pdf_p.content
+    text_part_list=[]
+    for i, row_block in enumerate(pdf_blocks):
+        row = row_block.content
+        row = row.lower()
+        row = " ".join(row.split())
         for target in targets:
-            if (
-                fuzz.partial_ratio(target.lower(), row.lower())
-                >= default_search_min_ratio
-            ):
-                text_part_list.append(Text_Block(Text_BlockType.TESTO, {}, pdf_p))
+            target_n = target.lower()
+            target_n = " ".join(target_n.split())
+            if target_n != "" and target_n in row:
+                if '%' in row:
+                    date_regex=r".*(\d{2}/\d{2}/\d{4})"
+                    interess_regex=r".*(\d+\.\d+%)"
+                    date_match=re.match(date_regex,pdf_blocks[i+1].content)
+                    if date_match:
+                        interess_match_l1=re.match(interess_regex,row)
+                        interess_match_l2=re.match(interess_regex,pdf_blocks[i+1].content)
+                        txt_blk=Text_Block(
+                            Text_BlockType.TARGET_BOND_2LINES,
+                            {
+                                "match": target,
+                                "nominal value": pdf_blocks[i-1].content,
+                                "date": date_match[1],
+                                "interest rate": interess_match_l1[1] if interess_match_l1 else interess_match_l2[1], 
+                                "currency": pdf_blocks[i+2].content,
+                                "acquisition cost": pdf_blocks[i+3].content,
+                                "carrying value": pdf_blocks[i+4].content,
+                                "% net assets": pdf_blocks[i+5].content
+                            },
+                            row_block
+                        )
+                    else:
+                        data_match=re.match(date_regex,row)
+                        interess_match=re.match(interess_regex,row)
+                        txt_blk=Text_Block(
+                            Text_BlockType.TARGET_BOND_LINE,
+                            {
+                                "match": target,
+                                "date": data_match[1],
+                                "interest rate": interess_match[1],
+                                "nominal value": pdf_blocks[i-1].content,
+                                "currency": pdf_blocks[i+1].content,
+                                "acquisition cost": pdf_blocks[i+2].content,
+                                "carrying value": pdf_blocks[i+3].content,
+                                "% net assets": pdf_blocks[i+4].content
+
+                            },
+                            row_block
+                        )
+                
+                else:
+                    txt_blk=Text_Block(
+                        Text_BlockType.TARGET_EQUITY_LINE,
+                        {
+                            "match": target,
+                            "nominal value": pdf_blocks[i-1].content,
+                            "currency": pdf_blocks[i+1].content,
+                            "acquisition cost": pdf_blocks[i+2].content,
+                            "carrying value": pdf_blocks[i+3].content,
+                            "% net assets": pdf_blocks[i+4].content
+                        },
+                        row_block
+                    )
+
+                text_part_list.append(
+                    txt_blk             
+                )
     return text_part_list
 
 
 def tabularize(text_block: Text_Block) -> dict:
-    complete_pattern = re.match(
-        r"(?P<nominal>[0-9.,]+)\s+(?P<company>.+?)\s*(?P<rate>\d[0-9.,]+%)\s*(?P<maturity>\d{2}/\d{2}/\d{4})\s+(?P<currency>[A-Z]{3})\s+(?P<acquisition>[0-9.,]+)\s+(?P<carrying>[0-9.,]+)\s+(?P<netassets>[0-9.,]+)",
-        text_block.content,
-    )
-    # Pattern senza interest rate e maturity
-    reduced_pattern = re.match(
-        r"(?P<nominal>[0-9.,]+)\s+(?P<company>.+?)\s+(?P<currency>[A-Z]{3})\s+(?P<acquisition>[0-9.,]+)\s+(?P<carrying>[0-9.,]+)\s+(?P<netassets>[0-9.,]+)",
-        text_block.content,
-    )
-    parsed_row = None
-    if complete_pattern:
-        parsed_row = {
-            "Matched Company": text_block.metadata["company_match"],
-            "Nominal Value": complete_pattern.group("nominal"),
-            "Company": complete_pattern.group("company"),
-            "Interest Rate": complete_pattern.group("rate"),
-            "Maturity": complete_pattern.group("maturity"),
-            "Currency": complete_pattern.group("currency"),
-            "Acquisition Cost": complete_pattern.group("acquisition"),
-            "Carrying Value": complete_pattern.group("carrying"),
-            "Net Assets": complete_pattern.group("netassets"),
-        }
-    elif reduced_pattern:
-        parsed_row = {
-            "Matched Company": text_block.metadata["company_match"],
-            "Nominal Value": reduced_pattern.group("nominal"),
-            "Company": reduced_pattern.group("company"),
-            "Interest Rate": None,
-            "Maturity": None,
-            "Currency": reduced_pattern.group("currency"),
-            "Acquisition Cost": reduced_pattern.group("acquisition"),
-            "Carrying Value": reduced_pattern.group("carrying"),
-            "Net Assets": reduced_pattern.group("netassets"),
-        }
-    else:
-        logger.warning("Row not recognized")
-        parsed_row = {}
-    return parsed_row
+    m=text_block.metadata
+    
+    date=None
+    interest_rate=None
+    if text_block.type_block == Text_BlockType.TARGET_BOND_2LINES or Text_BlockType.TARGET_BOND_LINE == text_block.type_block:
+        date=dt.datetime.strptime(''.join(m["date"].split()),"%d/%m/%Y")
+        interest_rate=float(''.join(m["interest rate"].split()).replace('%',''))/100
+
+    row={
+            "company": m["match"],
+            "date": date,
+            "interest rate": interest_rate,
+            "nominal value": int(''.join(m["nominal value"].replace(',','').split())),
+            "currency": ''.join(m["currency"].split()),
+            "acquisition cost": int(''.join(m["acquisition cost"].replace(',','').split())),
+            "carrying value":  int(''.join(m["carrying value"].replace(',','').split())),
+            "% net asset": float(''.join(m["% net assets"].split()).replace(',','.'))
+    }
+    return row
 
 
 class PDF_BlockType(Enum):
-    TITLE = auto()
-    TABLE = auto()
+    LINES_TABLE = auto()
 
 
 class Text_BlockType(Enum):
-    TESTO = auto()
+    TARGET_EQUITY_LINE = auto()
+    TARGET_BOND_LINE = auto()
+    TARGET_BOND_2LINES = auto()
