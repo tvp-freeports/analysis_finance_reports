4e4f5098b39480c159828479d17e09594ae26ec6
Angelo Serrecchia <angeloserrecchia.phys@gmail.com>
2025-07-04 14:10:28 +0200

    Aggiunto FIDEURAM-old + test

--- src/freeports_analysis/formats/FIDEURAM/__init__.py
+++ src/freeports_analysis/formats/FIDEURAM/__init__.py
@@ -1,4 +1,4 @@
-"""AMUNDI2 format submodule"""
+"""FIDEURAM format submodule"""
 
 from enum import Enum, auto
 import logging as log
@@ -10,7 +10,6 @@ from .. import PdfBlock, TextBlock
 from ..utils_pdf_filter import one_pdf_blk, standard_pdf_filtering
 from ..utils_text_extract import standard_text_extraction, equity_bond_blks
 from ..utils_deserialize import standard_deserialization
-from freeports_analysis.consts import Currency
 
 logger = log.getLogger(__name__)
 
@@ -26,22 +25,22 @@ class TextBlock:
 
 
 @standard_pdf_filtering(
-    header_txt="Titolo",
-    header_font="TrebuchetMS-Bold",
-    subfund_height=(None, 60),
-    subfund_font="Arial-BoldItalicMT",
-    body_font="TrebuchetMS",
-    y_range=(None, None),
+    header_txt="Country",
+    header_font="Arial",
+    subfund_height=(None, 82),
+    subfund_font="Arial-Bold",
+    body_font="Arial",
+    y_range=(103, 749),
 )
 def pdf_filter(xml_root, page) -> dict:
     pass
 
 
 @standard_text_extraction(
-    nominal_quantity_pos=+1,
-    market_value_pos=+4,
-    perc_net_assets_pos=+5,
-    currency=Currency.EUR,
+    nominal_quantity_pos=-1,
+    market_value_pos=+1,
+    perc_net_assets_pos=+2,
+    currency=-2,
     acquisition_cost_pos=None,
 )
 def text_extract(pdf_blocks, targets):
