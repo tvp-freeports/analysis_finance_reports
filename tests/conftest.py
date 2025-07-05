29922c85ddec863a7ab0d04a854daca61ff2bf0d
Angelo Serrecchia <angeloserrecchia.phys@gmail.com>
2025-07-05 17:29:11 +0200

    Added MEDIOLANUM test and url in url_mapping

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,7 @@ url_example_formats = {
     "ANIMA": "https://www.animasgr.it/d/EN/downloads/Documents/Anima%20Funds%20plc%2031%20December%202023%20Financials.pdf",
     "FIDEURAM": "https://www.fideuramassetmanagement.ie/upload/File/pdf/Policy_FAMI/WILLERFUNDS/DOC/FIDEURAM_WILLERFUNDS_Semi-Annual%2028.02.2023.pdf",
     "EURIZON_OLD": "https://www.fundsquare.net/download/dl?siteId=FSQ&v=8R3GVJJluMrT1vWzWKb2+2y6bAdM4PonP+u32Js1vq7RbUzoOUpWj9+xriJFNnBxAFS14hLTev85fvgpgbDFQEJJfw8puksMVK/oWiK71T9YU0KjYpAH3l1VIKUAV4rPjwPaPQ7DDY4yhqF+o4MlHw==",
+    "MEDIOLANUM": "https://www.mediolanumgestionefondi.it/static-assets/documenti/file/it/2025/05/02//Relazione_di_gestione_annuale_al_%2030122024.pdf",
 }
 
 single_page_tests = [
@@ -30,6 +31,7 @@ single_page_tests = [
     ("FIDEURAM", 51),
     ("FIDEURAM", 33),
     ("EURIZON_OLD", 97),
+    ("MEDIOLANUM", 55),
 ]
 
 all_pages_tests = [
@@ -40,4 +42,5 @@ all_pages_tests = [
     "AMUNDI2",
     "FIDEURAM",
     "EURIZON_OLD",
+    "MEDIOLANUM",
 ]
