1a2e24a83b59773ac38a7287f409ce705b7825a9
Angelo Serrecchia <angeloserrecchia.phys@gmail.com>
2025-07-05 16:51:38 +0200

    Added Eurizon_old test pag 97

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,6 +18,7 @@ url_example_formats = {
     "EURIZON": "https://www.eurizonam.hr/UserDocsImages//LUX/SAR_HR_en_LU1341630033_YES_2023-06-30.pdf",
     "ANIMA": "https://www.animasgr.it/d/EN/downloads/Documents/Anima%20Funds%20plc%2031%20December%202023%20Financials.pdf",
     "FIDEURAM": "https://www.fideuramassetmanagement.ie/upload/File/pdf/Policy_FAMI/WILLERFUNDS/DOC/FIDEURAM_WILLERFUNDS_Semi-Annual%2028.02.2023.pdf",
+    "EURIZON_OLD": "https://www.fundsquare.net/download/dl?siteId=FSQ&v=8R3GVJJluMrT1vWzWKb2+2y6bAdM4PonP+u32Js1vq7RbUzoOUpWj9+xriJFNnBxAFS14hLTev85fvgpgbDFQEJJfw8puksMVK/oWiK71T9YU0KjYpAH3l1VIKUAV4rPjwPaPQ7DDY4yhqF+o4MlHw==",
 }
 
 single_page_tests = [
@@ -28,7 +29,15 @@ single_page_tests = [
     ("AMUNDI2", 558),
     ("FIDEURAM", 51),
     ("FIDEURAM", 33),
-    # ("EURIZON_OLD", 97),
+    ("EURIZON_OLD", 97),
 ]
 
-all_pages_tests = ["ANIMA", "EURIZON", "AMUNDI", "ASTERIA", "AMUNDI2", "FIDEURAM"]
+all_pages_tests = [
+    "ANIMA",
+    "EURIZON",
+    "AMUNDI",
+    "ASTERIA",
+    "AMUNDI2",
+    "FIDEURAM",
+    "EURIZON_OLD",
+]
