73c7c3b6046f3d7234224418495a2aa2a01831b9
Angelo Serrecchia <angeloserrecchia.phys@gmail.com>
2025-07-24 20:49:46 +0200

    Solved issue optimal identification code for different formats

--- src/freeports_analysis/formats/mediolanum_it24/__init__.py
+++ src/freeports_analysis/formats/mediolanum_it24/__init__.py
@@ -1,38 +1,23 @@
-"""MEDIOLANUM_IT24 format submodule"""
+"""MEDIOLANUM3 submodule"""
 
-import logging as log
-from typing import TypeAlias
-from freeports_analysis.formats_utils.pdf_filter import (
-    OnePdfBlockType,
-    standard_pdf_filtering,
-    YRange,
-)
-from freeports_analysis.formats_utils.text_extract import (
-    standard_text_extraction,
-    EquityBondTextBlockType,
-)
+from freeports_analysis.formats_utils.pdf_filter import standard_pdf_filtering, YRange
+from freeports_analysis.formats_utils.text_extract import standard_text_extraction
 from freeports_analysis.formats_utils.deserialize import standard_deserialization
 from freeports_analysis.formats_utils.text_extract.match import (
     target_fuzzy_match,
     target_prefix_match,
 )
-from freeports_analysis.consts import Currency
-
-
-logger = log.getLogger(__name__)
-
-
-PdfBlockType: TypeAlias = OnePdfBlockType
-TextBlockType: TypeAlias = EquityBondTextBlockType
 
 
 @standard_pdf_filtering(
-    header_txt="Relazione di gestione al 30 dicembre 2024",
-    header_font="Helvetica",
-    subfund_height=YRange(None, 76),
-    subfund_font="Helvetica",
-    body_font="Helvetica",
-    y_range=(83, None),
+    header_txt="Descrizione",
+    header_font="TT5D22o00",
+    subfund_height=YRange(47, 62),
+    subfund_font="TT5CC2o00",
+    body_font=["TT5D42o00", "TT6162o00"],
+    y_range=None,
+    algorithm_flags=[False, False, False, False],
+    tolerance=0,
 )
 def pdf_filter(xml_root) -> dict:
     raise NotImplementedError
@@ -40,11 +25,11 @@ def pdf_filter(xml_root) -> dict:
 
 @standard_text_extraction(
     nominal_quantity_pos=+1,
-    market_value_pos=+2,
-    perc_net_assets_pos=+3,
-    currency=Currency.EUR,
-    acquisition_cost_pos=None,
-    match_func=lambda x, y: target_fuzzy_match(x, y, 0.65)
+    market_value_pos=+4,
+    perc_net_assets_pos=+5,
+    currency=+2,
+    acquisition_cost_pos=+3,
+    match_func=lambda x, y: target_fuzzy_match(x, y, 0.8)
     and target_prefix_match(x, y, 0.3),
 )
 def text_extract(pdf_blocks, targets):
