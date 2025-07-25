ecc4f8a630adc9252477827f4a0cb223946bb75b
Angelo Serrecchia <angeloserrecchia.phys@gmail.com>
2025-07-25 12:37:13 +0200

    Added size functions in font.py

--- src/freeports_analysis/formats/mediolanum_it24/__init__.py
+++ src/freeports_analysis/formats/mediolanum_it24/__init__.py
@@ -1,12 +1,12 @@
 """MEDIOLLANUM_IT24 format submodule"""
 
-from freeports_analysis.formats_utils.pdf_filter import standard_pdf_filtering, YRange
+from freeports_analysis.formats_utils.pdf_filter import (
+    standard_pdf_filtering,
+    YRange,
+    is_present_txt_font,
+)
 from freeports_analysis.formats_utils.text_extract import standard_text_extraction
 from freeports_analysis.formats_utils.deserialize import standard_deserialization
-from freeports_analysis.formats_utils.text_extract.match import (
-    target_fuzzy_match,
-    target_prefix_match,
-)
 
 
 @standard_pdf_filtering(
@@ -14,13 +14,34 @@ from freeports_analysis.formats_utils.text_extract.match import (
     header_font="TT5D22o00",
     subfund_height=YRange(47, 62),
     subfund_font="TT5CC2o00",
-    body_font=["TT5D42o00", "TT6162o00"],
+    body_font="TT5D42o00",
     y_range=None,
     algorithm_flags=[False, False, False, False],
     tolerance=0,
 )
-def pdf_filter(xml_root) -> dict:
+def pdf_filter_1(xml_root) -> dict:
     raise NotImplementedError
+
+
+# bond
+@standard_pdf_filtering(
+    header_txt="Descrizione",
+    header_font="TT6142o00",
+    subfund_height=YRange(47, 62),
+    subfund_font="TT60C2o00",
+    body_font="TT6162o00",
+    y_range=None,
+    algorithm_flags=[False, False, False, False],
+    tolerance=0,
+)
+def pdf_filter_2(xml_root) -> dict:
+    raise NotImplementedError
+
+
+def pdf_filter(xml_root) -> dict:
+    if is_present_txt_font(xml_root, "Descrizione", "TT5D22o00"):
+        return pdf_filter_1(xml_root)
+    return pdf_filter_2(xml_root)
 
 
 @standard_text_extraction(
@@ -29,8 +50,6 @@ def pdf_filter(xml_root) -> dict:
     perc_net_assets_pos=+5,
     currency=+2,
     acquisition_cost_pos=+3,
-    match_func=lambda x, y: target_fuzzy_match(x, y, 0.8)
-    and target_prefix_match(x, y, 0.3),
 )
 def text_extract(pdf_blocks, targets):
     raise NotImplementedError
--- src/freeports_analysis/formats_utils/pdf_filter/xml/font.py
+++ src/freeports_analysis/formats_utils/pdf_filter/xml/font.py
@@ -4,6 +4,9 @@ from typing import List, Union
 from lxml import etree
 
 
+# FONT
+
+
 def is_present_txt_font(blk: etree.Element, txt: str, font: str) -> bool:
     """Return if a certain pdf block with a specific text and font is present in the tree
 
@@ -80,3 +83,75 @@ def get_lines_with_font(
     xpath_query = f"./descendant-or-self::line[{font_conditions}]"
 
     return blk.xpath(xpath_query)
+
+
+# SIZE
+
+
+def get_lines_with_size(blk: etree.Element, size: str) -> List[etree.Element]:
+    """
+    Return all the lines with a specific font size in a tree.
+
+    Parameters
+    ----------
+    blk : etree.Element
+        Tree from which to extract lines.
+    size : str
+        Font size to match (as a string).
+
+    Returns
+    -------
+    List[etree.Element]
+        List of <line> elements that contain a <font> with the specified size.
+    """
+
+    return blk.xpath(f".//line[font[@size='{size}']]")
+
+
+def get_lines_with_txt_size(
+    blk: etree.Element, txt: str, size: str, all_elem: bool = False
+) -> List[etree.Element] | etree.Element:
+    """Get lines with a certain txt and size
+
+    Parameters
+    ----------
+    blk : etree.Element
+        xml tree structure
+    txt : str
+        text to search for
+    size : str
+        size to search for
+    all_elem : bool, optional
+        if `True` return a list with all matches, if `False` just the first
+        as a scalar element
+
+    Returns
+    -------
+    List[etree.Element] | etree.Element
+        matching lines
+    """
+    blks = blk.xpath(
+        f"./descendant-or-self::line[contains(@text,'{txt}') and font[@size='{size}']]"
+    )
+    return blks if all_elem else blks[0] if len(blks) > 0 else None
+
+
+def is_present_txt_size(blk: etree.Element, txt: str, size: str) -> bool:
+    """Return if a certain pdf block with a specific text and size is present in the tree
+
+    Parameters
+    ----------
+    blk : etree.Element
+        tree to search in
+    txt : str
+        text to search
+    size : str
+        size to search
+
+    Returns
+    -------
+    bool
+        boolean describing if the block is present or not
+    """
+    relevant_part = get_lines_with_txt_size(blk, txt, size, all_elem=True)
+    return len(relevant_part) > 0
