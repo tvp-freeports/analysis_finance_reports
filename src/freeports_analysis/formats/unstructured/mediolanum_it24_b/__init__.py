f28b55b11f434015f03be7dc4cb2d9d8ff844173
Giuliano Verrando <giulianoverrando@gmail.com>
2025-08-28 11:34:39 +0200

    added mediolanum b

--- src/freeports_analysis/formats/unstructured/mediolanum_it24_b/__init__.py
+++ src/freeports_analysis/formats/unstructured/mediolanum_it24_b/__init__.py
@@ -4,8 +4,6 @@ from freeports_analysis.formats_utils.pdf_filter import (
     standard_pdf_filtering,
     PdfLineSet,
 )
-from freeports_analysis.formats_utils.text_extract import standard_text_extraction
-from freeports_analysis.formats_utils.deserialize import standard_deserialization
 from freeports_analysis.consts import Currency
 from freeports_analysis.formats_utils.pdf_filter.pdf_parts import XRange, YRange, Area
 from freeports_analysis.formats_utils.pdf_filter.xml.font import get_lines_with_txt_font
@@ -38,15 +36,3 @@ def pdf_filter(xml_root):
         raise NotImplementedError
 
     return _pdf_filter(xml_root)
-
-
-@standard_text_extraction(
-    nominal_quantity_pos=+1, market_value_pos=+2, perc_net_assets_pos=+3
-)
-def text_extract(pdf_blocks, targets):
-    raise NotImplementedError
-
-
-@standard_deserialization()
-def deserialize(text_block, targets):
-    raise NotImplementedError
