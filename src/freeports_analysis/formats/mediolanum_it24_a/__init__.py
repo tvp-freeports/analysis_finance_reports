a654208de83eae58a02df32e18fa94cd7b084f43
Giuliano Verrando <giulianoverrando@gmail.com>
2025-08-29 20:55:43 +0200

    Font range in mediolanum it24

--- src/freeports_analysis/formats/mediolanum_it24_a/__init__.py
+++ src/freeports_analysis/formats/mediolanum_it24_a/__init__.py
@@ -6,16 +6,21 @@ from freeports_analysis.formats_utils.pdf_filter import (
 )
 from freeports_analysis.formats_utils.text_extract import standard_text_extraction
 from freeports_analysis.formats_utils.deserialize import standard_deserialization
+from freeports_analysis.formats_utils.pdf_filter.pdf_parts.font import FontSizeRange
 
 
 @standard_pdf_filtering(
     header_set=[
-        PdfLineSet(font_size=8.037657, text="Descrizione"),
-        PdfLineSet(font_size=15.47555, text="TABELLA DEGLI INVESTIMENTI AL"),
+        PdfLineSet(font_size=FontSizeRange(7.7, 8.1), text="Descrizione"),
+        PdfLineSet(
+            font_size=FontSizeRange(15.45, 15.55), text="TABELLA DEGLI INVESTIMENTI AL"
+        ),
     ],
-    subfund_set=PdfLineSet(font_size=15.47555, area=(0, 80)),
-    body_set=PdfLineSet(font_size=8.037657),
-    currency_set=PdfLineSet(font_size=8.037657, text="valori espressi in"),
+    subfund_set=PdfLineSet(font_size=FontSizeRange(15.45, 15.55), area=(0, 80)),
+    body_set=PdfLineSet(font_size=FontSizeRange(7.7, 8.1)),
+    currency_set=PdfLineSet(
+        font_size=FontSizeRange(7.7, 8.1), text="valori espressi in"
+    ),
     deselection_list=[
         PdfLineSet(text="TABELLA DEGLI INVESTIMENTI AL"),
         PdfLineSet(text="^ "),
--- src/freeports_analysis/formats_utils/pdf_filter/pdf_parts/__init__.py
+++ src/freeports_analysis/formats_utils/pdf_filter/pdf_parts/__init__.py
@@ -3,7 +3,7 @@
 from typing import Optional, Tuple
 from lxml import etree
 from freeports_analysis.i18n import _
-from .font import Font, FontSize
+from .font import Font, FontSize, FontSizeRange
 from ..xml.position import get_bounds
 from .position import Area, XRange, YRange, Coord
 
@@ -221,8 +221,10 @@ class PdfLineSet(PdfLine):
             if not begin and not end:
                 eq = eq and (effective_text in other.text)
         eq = eq and (self.font is None or self.font == other.font)
-        eq = eq and (
-            self.font_size is None or abs(self.font_size - other.font_size) <= 1e-4
-        )
+        if self.font_size is not None:
+            if isinstance(self.font_size, FontSizeRange):
+                eq = eq and other.font_size in self.font_size
+            else:
+                eq = eq and abs(self.font_size - other.font_size) <= 1e-4
         eq = eq and (self.geometry is None or self.geometry in other.geometry)
         return eq
