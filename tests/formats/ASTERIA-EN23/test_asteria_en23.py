127273fb4d467e8d6ef61148d85892263f4fcb2d
Giuliano Verrando <giulianoverrando@gmail.com>
2025-09-09 18:53:57 +0200

    added steria en 23 tests

---
+++ tests/formats/ASTERIA-EN23/test_asteria_en23.py
@@ -0,0 +1,30 @@
+import pytest
+from tests.formats import (
+    generic_test_pdf_filter,
+    generic_test_text_extract,
+    generic_test_deserialize,
+    generic_test_pipelines,
+    get_pages,
+)
+
+pages = get_pages(__file__)
+
+
+@pytest.mark.parametrize("page", pages)
+def test_pdf_filter(page):
+    generic_test_pdf_filter(page, __file__)
+
+
+@pytest.mark.parametrize("page", pages)
+def test_text_extract(page):
+    generic_test_text_extract(page, __file__)
+
+
+@pytest.mark.parametrize("page", pages)
+def test_deserialize(page):
+    generic_test_deserialize(page, __file__)
+
+
+@pytest.mark.integration_tests
+def test_pipeline():
+    generic_test_pipelines(__file__)
