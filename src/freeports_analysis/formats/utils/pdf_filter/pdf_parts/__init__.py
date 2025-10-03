5d7bbed268ec815fccadbe04c6d2705bcdd3a2b3
Giuliano Verrando <giulianoverrando@gmail.com>
2025-10-03 18:33:49 +0200

    added aggregators

--- src/freeports_analysis/formats/utils/pdf_filter/pdf_parts/__init__.py
+++ src/freeports_analysis/formats/utils/pdf_filter/pdf_parts/__init__.py
@@ -169,134 +169,6 @@ line_set_regexp += f"({_line_set_text_regexp})?"
 _line_set_regexp = re.compile(line_set_regexp)
 
 
-def _pdf_line_set_aggregator(attribute):
-    def wrapper(agg_func):
-        def new_agg(value, lines, xml_root):
-            value = value.contextualize(xml_root)
-            inputs = [getattr(l, attribute) for l in lines if l in value]
-            return agg_func(inputs)
-
-        return new_agg
-
-    return wrapper
-
-
-@_pdf_line_set_aggregator("font")
-def _default_font_agg(fonts):
-    return FontSet(*fonts)
-
-
-@_pdf_line_set_aggregator("font_size")
-def _default_font_size_agg(font_sizes):
-    if len(font_sizes) == 1:
-        fs = font_sizes[0]
-        font_sizes = [fs - 1e-4, fs + 1e-4]
-    return FontSizeSet.from_range(min(*font_sizes), max(*font_sizes))
-
-
-@_pdf_line_set_aggregator("text")
-def _default_text_agg(texts):
-    return TextSet(*[f"^{t}$" for t in texts])
-
-
-@_pdf_line_set_aggregator("area")
-def _pdflineset_area_agg(areas):
-    if len(areas) == 1:
-        y_max = areas[0].bounds[1]
-        return box(-1e6, -1e6, 1e6, y_max)
-    if len(areas) == 2:
-        x_min0, y_min0, x_max0, y_max0 = areas[0].bounds
-        x_min1, y_min1, x_max1, y_max1 = areas[1].bounds
-        h_a = y_max1 - y_min2
-        h_b = y_max2 - y_min1
-        h = min(abs(h_a), abs(h_b))
-        w_a = x_max1 - x_min2
-        w_b = x_max2 - x_min1
-        w = min(abs(w_a), abs(w_b))
-        if h > w:
-            return box(-1e6, min(y_max1, y_max2), 1e6, max(y_min1, y_min2))
-        return box(min(x_max1, x_max2), -1e6, min(x_min1, x_min2), 1e6)
-    if len(areas) == 3:
-        x_min0, y_min0, x_max0, y_max0 = areas[0].bounds
-        x_min1, y_min1, x_max1, y_max1 = areas[1].bounds
-        x_min2, y_min2, x_max2, y_max2 = areas[2].bounds
-        h_a12 = y_max1 - y_min2
-        h_b12 = y_max2 - y_min1
-        h_a23 = y_max2 - y_min3
-        h_b23 = y_max3 - y_min2
-        h_a13 = y_max1 - y_min3
-        h_b13 = y_max3 - y_min1
-        h_12 = min(abs(h_a12), abs(h_b12))
-        h_23 = min(abs(h_a23), abs(h_b23))
-        h_13 = min(abs(h_a13), abs(h_b13))
-        i_h, h = max((0, h_12), (1, h_23), (2, h_13), key=lambda x: x[1])
-
-        w_a12 = x_max1 - x_min2
-        w_b12 = x_max2 - x_min1
-        w_a23 = x_max2 - x_min3
-        w_b23 = x_max3 - x_min2
-        w_a13 = x_max1 - x_min3
-        w_b13 = x_max3 - x_min1
-        w_12 = min(abs(w_a12), abs(w_b12))
-        w_23 = min(abs(w_a23), abs(w_b23))
-        w_13 = min(abs(w_a13), abs(w_b13))
-        i_w, w = max((0, w_12), (1, w_23), (2, w_13), key=lambda x: x[1])
-        if h > w:
-            y_maxs = [(y_max1, y_max2), (y_max2, y_max3), (y_max1, y_max3)]
-            y_mins = [(y_min1, y_min2), (y_min2, y_min3), (y_min1, y_min3)]
-            other_x = [(x_min3, x_max3), (x_min1, x_max1), (x_min2, x_max2)]
-            x_mins, x_maxs = tuple(zip(*other_x))
-            return box(
-                other_x[i_h][1] if other_x[i_h][1] == min(*x_maxs) else -1e6,
-                min(*y_maxs[i_h]),
-                other_x[i_h][0] if other_x[i_h][0] == max(*x_mins) else 1e6,
-                max(*y_mins[i_h]),
-            )
-        x_maxs = [(x_max1, x_max2), (x_max2, x_max3), (x_max1, x_max3)]
-        x_mins = [(x_min1, x_min2), (x_min2, x_min3), (x_min1, x_min3)]
-        other_y = [(y_min3, y_max3), (y_min1, y_max1), (y_min2, y_max2)]
-        y_mins, y_maxs = tuple(zip(*other_y))
-        return box(
-            min(*x_maxs[i_w]),
-            other_y[i_w][1] if other_y[i_w][1] == min(*y_maxs) else -1e6,
-            max(*x_mins[i_w]),
-            other_y[i_w][0] if other_y[i_w][0] == max(*y_mins) else 1e6,
-        )
-
-    bounds = [a.bounds for a in areas]
-    x_mins, y_mins, x_maxs, y_maxs = tuple(zip(*bounds))
-    return box(min(*x_maxs), min(*y_maxs), max(*x_mins), max(*y_mins))
-
-
-def _default_area_agg(value, lines, xml_root):
-    concrete_values = {"x_min": None, "x_max": None, "y_min": None, "y_max": None}
-    for k in concrete_values.keys():
-        vl = value[k]
-        if isinstace(vl, PdfLineSet):
-            vl = vl.contextualize(xml_root)
-            bounds = [l.area.bounds for l in lines if l in vl]
-            bounds_t = tuple(zip(*bounds))
-            vl = {
-                "x_min": min(*bounds_t[2]),
-                "y_min": min(*bounds_t[3]),
-                "x_max": max(*bounds_t[0]),
-                "y_max": max(*bounds_t[1]),
-            }[k]
-        concrete_values[k] = vl
-    return box(
-        concrete_values["x_min"],
-        concrete_values["y_min"],
-        concrete_values["x_max"],
-        concrete_values["y_max"],
-    )
-
-
-_font_aggregators = [(PdfLineSet, _default_font_agg)]
-_font_size_aggregators = [(PdfLineSet, _default_font_size_agg)]
-_text_aggregators = [(PdfLineSet, _default_text_agg)]
-_area_aggregators = [(dict, _default_area_agg), (PdfLineSet, _pdflineset_area_agg)]
-
-
 class PdfLineSet:
     def __init__(
         self,
@@ -469,14 +341,7 @@ class PdfLineSet:
             string += f"\t{area}\n"
         return string
 
-    def contextualize(
-        self,
-        xml_root,
-        area_agg_func=_default_area_agg,
-        font_size_agg_func=_default_font_size_agg,
-        text_agg_func=_default_text_agg,
-        font_agg_func=_default_font_agg,
-    ):
+    def contextualize(self, xml_root):
         concrete = PdfLineSet(
             font=self.font, font_size=self.font_size, area=self.area, text=self.text
         )
@@ -499,16 +364,148 @@ class PdfLineSet:
                 )
             )
 
+        font_aggregators = _font_aggregators
+        font_size_aggregators = _font_size_aggregators
+        text_aggregators = _text_aggregators
+        area_aggregators = _area_aggregators
         concrete._font = _contextualize(
-            Font, concrete.font, _font_aggregators, lines, xml_root
+            Font, concrete.font, font_aggregators, lines, xml_root
         )
         concrete._font_size = _contextualize(
-            FontSizeSet, concrete.font_size, _font_size_aggregators, lines, xml_root
+            FontSizeSet, concrete.font_size, font_size_aggregators, lines, xml_root
         )
         concrete._text = _contextualize(
-            TextSet, concrete.text, _text_aggregators, lines, xml_root
+            TextSet, concrete.text, text_aggregators, lines, xml_root
         )
         concrete._area = _contextualize(
-            Polygon, concrete.area, _area_aggregators, lines, xml_root
+            Polygon, concrete.area, area_aggregators, lines, xml_root
         )
         return concrete
+
+
+def _pdf_line_set_aggregator(attribute):
+    def wrapper(agg_func):
+        def new_agg(value, lines, xml_root):
+            value = value.contextualize(xml_root)
+            inputs = [getattr(l, attribute) for l in lines if l in value]
+            return agg_func(inputs)
+
+        return new_agg
+
+    return wrapper
+
+
+@_pdf_line_set_aggregator("font")
+def _default_font_agg(fonts):
+    return FontSet(*fonts)
+
+
+@_pdf_line_set_aggregator("font_size")
+def _default_font_size_agg(font_sizes):
+    if len(font_sizes) == 1:
+        fs = font_sizes[0]
+        font_sizes = [fs - 1e-4, fs + 1e-4]
+    return FontSizeSet.from_range(min(*font_sizes), max(*font_sizes))
+
+
+@_pdf_line_set_aggregator("text")
+def _default_text_agg(texts):
+    return TextSet(*[f"^{t}$" for t in texts])
+
+
+@_pdf_line_set_aggregator("area")
+def _pdflineset_area_agg(areas):
+    if len(areas) == 1:
+        y_max = areas[0].bounds[1]
+        return box(-1e6, -1e6, 1e6, y_max)
+    if len(areas) == 2:
+        x_min0, y_min0, x_max0, y_max0 = areas[0].bounds
+        x_min1, y_min1, x_max1, y_max1 = areas[1].bounds
+        h_a = y_max1 - y_min2
+        h_b = y_max2 - y_min1
+        h = min(abs(h_a), abs(h_b))
+        w_a = x_max1 - x_min2
+        w_b = x_max2 - x_min1
+        w = min(abs(w_a), abs(w_b))
+        if h > w:
+            return box(-1e6, min(y_max1, y_max2), 1e6, max(y_min1, y_min2))
+        return box(min(x_max1, x_max2), -1e6, min(x_min1, x_min2), 1e6)
+    if len(areas) == 3:
+        x_min0, y_min0, x_max0, y_max0 = areas[0].bounds
+        x_min1, y_min1, x_max1, y_max1 = areas[1].bounds
+        x_min2, y_min2, x_max2, y_max2 = areas[2].bounds
+        h_a12 = y_max1 - y_min2
+        h_b12 = y_max2 - y_min1
+        h_a23 = y_max2 - y_min3
+        h_b23 = y_max3 - y_min2
+        h_a13 = y_max1 - y_min3
+        h_b13 = y_max3 - y_min1
+        h_12 = min(abs(h_a12), abs(h_b12))
+        h_23 = min(abs(h_a23), abs(h_b23))
+        h_13 = min(abs(h_a13), abs(h_b13))
+        i_h, h = max((0, h_12), (1, h_23), (2, h_13), key=lambda x: x[1])
+
+        w_a12 = x_max1 - x_min2
+        w_b12 = x_max2 - x_min1
+        w_a23 = x_max2 - x_min3
+        w_b23 = x_max3 - x_min2
+        w_a13 = x_max1 - x_min3
+        w_b13 = x_max3 - x_min1
+        w_12 = min(abs(w_a12), abs(w_b12))
+        w_23 = min(abs(w_a23), abs(w_b23))
+        w_13 = min(abs(w_a13), abs(w_b13))
+        i_w, w = max((0, w_12), (1, w_23), (2, w_13), key=lambda x: x[1])
+        if h > w:
+            y_maxs = [(y_max1, y_max2), (y_max2, y_max3), (y_max1, y_max3)]
+            y_mins = [(y_min1, y_min2), (y_min2, y_min3), (y_min1, y_min3)]
+            other_x = [(x_min3, x_max3), (x_min1, x_max1), (x_min2, x_max2)]
+            x_mins, x_maxs = tuple(zip(*other_x))
+            return box(
+                other_x[i_h][1] if other_x[i_h][1] == min(*x_maxs) else -1e6,
+                min(*y_maxs[i_h]),
+                other_x[i_h][0] if other_x[i_h][0] == max(*x_mins) else 1e6,
+                max(*y_mins[i_h]),
+            )
+        x_maxs = [(x_max1, x_max2), (x_max2, x_max3), (x_max1, x_max3)]
+        x_mins = [(x_min1, x_min2), (x_min2, x_min3), (x_min1, x_min3)]
+        other_y = [(y_min3, y_max3), (y_min1, y_max1), (y_min2, y_max2)]
+        y_mins, y_maxs = tuple(zip(*other_y))
+        return box(
+            min(*x_maxs[i_w]),
+            other_y[i_w][1] if other_y[i_w][1] == min(*y_maxs) else -1e6,
+            max(*x_mins[i_w]),
+            other_y[i_w][0] if other_y[i_w][0] == max(*y_mins) else 1e6,
+        )
+
+    bounds = [a.bounds for a in areas]
+    x_mins, y_mins, x_maxs, y_maxs = tuple(zip(*bounds))
+    return box(min(*x_maxs), min(*y_maxs), max(*x_mins), max(*y_mins))
+
+
+def _default_area_agg(value, lines, xml_root):
+    concrete_values = {"x_min": None, "x_max": None, "y_min": None, "y_max": None}
+    for k in concrete_values.keys():
+        vl = value[k]
+        if isinstace(vl, PdfLineSet):
+            vl = vl.contextualize(xml_root)
+            bounds = [l.area.bounds for l in lines if l in vl]
+            bounds_t = tuple(zip(*bounds))
+            vl = {
+                "x_min": min(*bounds_t[2]),
+                "y_min": min(*bounds_t[3]),
+                "x_max": max(*bounds_t[0]),
+                "y_max": max(*bounds_t[1]),
+            }[k]
+        concrete_values[k] = vl
+    return box(
+        concrete_values["x_min"],
+        concrete_values["y_min"],
+        concrete_values["x_max"],
+        concrete_values["y_max"],
+    )
+
+
+_font_aggregators = [(PdfLineSet, _default_font_agg)]
+_font_size_aggregators = [(PdfLineSet, _default_font_size_agg)]
+_text_aggregators = [(PdfLineSet, _default_text_agg)]
+_area_aggregators = [(dict, _default_area_agg), (PdfLineSet, _pdflineset_area_agg)]
