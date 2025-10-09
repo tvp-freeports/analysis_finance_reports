790b6782db84bfb88400f0e359f6484ed3d17ff0
Giuliano Verrando <giulianoverrando@gmail.com>
2025-10-09 12:20:04 +0200

    CARNE-EN23 test added no warn

---
+++ src/freeports_analysis/formats/algorithms/unstructured/carne_en23.py
@@ -0,0 +1,25 @@
+from freeports_analysis.formats.utils.pdf_filter import standard_pdf_filtering
+from freeports_analysis.formats.utils.pdf_filter.pdf_parts import PdfLineSet
+
+header_set = [
+    PdfLineSet.from_str('Arial-BoldMT "Description"'),
+    PdfLineSet.from_str('Arial-BoldMT "Currency"'),
+]
+subfund_set = PdfLineSet.from_str("ArialMT[6.96]")
+subfund_set._left._area = (
+    PdfLineSet.from_str('ArialMT[6.96] "^Annual report including"'),
+    (0, 0.8),
+    (1.1, 1.4),
+)
+currency_set = PdfLineSet.from_str('Arial-BoldMT "Valuation in"')
+body_set = PdfLineSet.from_str("ArialMT[6.96](160:786)")
+
+
+@standard_pdf_filtering(
+    header_set=header_set,
+    subfund_set=subfund_set,
+    currency_set=currency_set,
+    body_set=body_set,
+)
+def pdf_filter(xml_root):
+    pass
