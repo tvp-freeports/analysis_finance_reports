d053a46922d211b539b40c3fe810b989bac3aa89
Giuliano Verrando <giulianoverrando@gmail.com>
2026-02-09 22:45:25 +0100

    added all tests for subtract subset

--- rust/src/pdf_filter/select/pdf_line/area.rs
+++ rust/src/pdf_filter/select/pdf_line/area.rs
@@ -44,7 +44,9 @@ enum RectOverlapping {
     TopLeft,
     TopRight,
     BottomLeft,
-    BottomRight
+    BottomRight,
+    Vertical,
+    Horizontal
 }
 
 impl Rectangle {
@@ -53,7 +55,7 @@ impl Rectangle {
         let (x0,y0,x1,y1) = self.as_tuple();
         let (a0,b0,a1,b1) = other.as_tuple();
         if x0<=a0 {
-            // BiggerRight SmallerRight TopRight BottomRight SmallerTop SmallerBottom
+            // BiggerRight SmallerRight TopRight BottomRight SmallerTop SmallerBottom Vertical
             if x1 <= a1 {
                 // BiggerRight SmallerRight TopRight BottomRight
                 if y0<=b0 {
@@ -72,15 +74,20 @@ impl Rectangle {
                     }
                 }
             } else {
-                // SmallerTop SmallerBottom
+                // SmallerTop SmallerBottom Vertical
                 if b0 <= y0 {
-                    SmallerTop
+                    // SmallerTop Vertical
+                    if b1 <= y1 {
+                        SmallerTop
+                    } else {
+                        Vertical
+                    } 
                 } else {
                     SmallerBottom
                 }
             }
         } else {
-            // BiggerLeft SmallerLeft TopLeft BottomLeft BiggerTop BiggerBottom
+            // BiggerLeft SmallerLeft TopLeft BottomLeft BiggerTop BiggerBottom Horizontal
             if a1<=x1 {
                 // BiggerLeft SmallerLeft TopLeft BottomLeft
                 if y0<=b0 {
@@ -99,89 +106,26 @@ impl Rectangle {
                     }
                 }
             } else {
-                // BiggerTop BiggerBottom
+                // BiggerTop BiggerBottom Horizontal
                 if b1 <= y1 {
-                    BiggerTop
+                    // BiggerTop Horizontal
+                    if y0 <= b0 {
+                        Horizontal
+                    } else {
+                        BiggerTop
+                    }
                 } else {
                     BiggerBottom
                 }
             }
         }
     }
-}
-
-
-pub enum SubtractOverlappingRectanglesRes {
-    One(Rectangle),
-    Two(Rectangle,Rectangle),
-    Three(Rectangle,Rectangle,Rectangle)
-}
-pub enum SubtractSubsetRectanglesRes {
-    Four(Rectangle,Rectangle,Rectangle,Rectangle)
-}
-pub enum IntersectOverlappingRectanglesRes {
-    One(Rectangle)
-}
-
-impl Into<CompoundAtomOperationRes<Rectangle>> for SubtractSubsetRectanglesRes {
-    fn into(self) -> CompoundAtomOperationRes<Rectangle> {
-        use SubtractSubsetRectanglesRes::*;
-        match self {
-            Four(a,b,c,d) => CompoundAtomOperationRes::Four(a,b,c,d)
-        }
-    }
-}
-impl Into<CompoundAtomOperationRes<Rectangle>> for SubtractOverlappingRectanglesRes {
-    fn into(self) -> CompoundAtomOperationRes<Rectangle> {
-        use SubtractOverlappingRectanglesRes::*;
-        match self {
-            One(a) => CompoundAtomOperationRes::One(a),
-            Two(a,b) => CompoundAtomOperationRes::Two(a,b),
-            Three(a,b,c) => CompoundAtomOperationRes::Three(a,b,c)
-        }
-    }
-}
-impl Into<CompoundAtomOperationRes<Rectangle>> for IntersectOverlappingRectanglesRes {
-    fn into(self) -> CompoundAtomOperationRes<Rectangle> {
-        use IntersectOverlappingRectanglesRes::*;
-        match self {
-            One(a) => CompoundAtomOperationRes::One(a)
-        }
-    }
-}
-
-
-impl AtomOperations for  Rectangle {
-    type SubtractOverlappingRes = SubtractOverlappingRectanglesRes;
-    type SubtractSubsetRes = SubtractSubsetRectanglesRes;
-    type IntersectOverlappingRes = IntersectOverlappingRectanglesRes;
-    
-    fn subtract_subset(&self,other: &Self) -> SubtractSubsetRectanglesRes {
-        use SubtractSubsetRectanglesRes::*;
-        let (x0,y0,x1,y1) = self.as_tuple();
-        let (a0,b0,a1,b1) = other.as_tuple();
-        let h: (f32,f32,f32,f32);
-        let v: (f32,f32,f32,f32);
-        if a0<x0 || b0<y0 || a1<x1 || b1<y1 {
-            h=(a0,x0,x1,a1);
-            v=(b0,y0,y1,b1);
-        } else {
-            h=(x0,a0,a1,x1);
-            v=(y0,b0,b1,y1);
-        }
-        Four(
-            Self::new(h.0,v.0,h.2,v.1),
-            Self::new(h.0,v.1,h.1,v.3),
-            Self::new(h.1,v.2,h.3,v.3),
-            Self::new(h.2,v.0,h.3,v.2)
-        )
-    }
-    fn subtract_overlapping(&self,other: &Self) -> SubtractOverlappingRectanglesRes {
+    fn subtract_as_overlap_type(&self,other: &Self, ovrlt: RectOverlapping) -> SubtractOverlappingRectanglesRes {
         use SubtractOverlappingRectanglesRes::*;
         use RectOverlapping::*;
         let (x0,y0,x1,y1) = self.as_tuple();
         let (a0,b0,a1,b1) = other.as_tuple();
-        match self.type_overlap(other) {
+        match ovrlt {
             SmallerLeft => Three(
                 Self::new(x0,y0,x1,b0),
                 Self::new(a1,b0,x1,y1),
@@ -211,18 +155,102 @@ impl AtomOperations for  Rectangle {
                 Self::new(x0,b1,a1,y1)
             ),
             TopRight => Two(
-                Self::new(x0,y0,x1,b0),
-                Self::new(a1,b0,x1,y1)
+                Self::new(x0,b1,x1,y1),
+                Self::new(x0,y0,a0,b1)
             ),
             BottomRight => Two(
                 Self::new(x0,y0,a0,y1),
                 Self::new(a0,y0,x1,b0)
             ),
             BottomLeft => Two(
-                Self::new(x0,b1,x1,y1),
-                Self::new(x0,y0,a0,b1)
+                Self::new(x0,y0,x1,b0),
+                Self::new(a1,b0,x1,y1)
+            ),
+            Vertical => Two(
+                Self::new(x0,y0,a0,y1),
+                Self::new(a1,y0,x1,y1)
+            ),
+            Horizontal => Two(
+                Self::new(x0,y0,x1,b0),
+                Self::new(x0,b1,x1,y1)
             )
         }
+    }
+}
+
+
+pub enum SubtractOverlappingRectanglesRes {
+    One(Rectangle),
+    Two(Rectangle,Rectangle),
+    Three(Rectangle,Rectangle,Rectangle)
+}
+pub enum SubtractSubsetRectanglesRes {
+    Three(Rectangle,Rectangle,Rectangle),
+    Four(Rectangle,Rectangle,Rectangle,Rectangle)
+}
+pub enum IntersectOverlappingRectanglesRes {
+    One(Rectangle)
+}
+
+impl Into<CompoundAtomOperationRes<Rectangle>> for SubtractOverlappingRectanglesRes {
+    fn into(self) -> CompoundAtomOperationRes<Rectangle> {
+        use CompoundAtomOperationRes::*;
+        match self {
+            Self::One(a) => One(a),
+            Self::Two(a,b) => Two(a,b),
+            Self::Three(a,b,c) => Three(a,b,c)
+        }
+    }
+}
+impl Into<CompoundAtomOperationRes<Rectangle>> for IntersectOverlappingRectanglesRes {
+    fn into(self) -> CompoundAtomOperationRes<Rectangle> {
+        use CompoundAtomOperationRes::*;
+        match self {
+            Self::One(a) => One(a)
+        }
+    }
+}
+
+
+impl AtomOperations for  Rectangle {
+    type SubtractOverlappingRes = SubtractOverlappingRectanglesRes;
+    type SubtractSubsetRes = CompoundAtomOperationRes<Rectangle>;
+    type IntersectOverlappingRes = IntersectOverlappingRectanglesRes;
+    
+    fn subtract_subset(&self,other: &Self) -> CompoundAtomOperationRes<Rectangle> {
+        use CompoundAtomOperationRes::*;
+        use RectOverlapping::*;
+        let (x0,y0,x1,y1) = self.as_tuple();
+        let (a0,b0,a1,b1) = other.as_tuple();
+        let h = (x0,a0,a1,x1);
+        let v = (y0,b0,b1,y1);
+        match (x0==a0,y0==b0,x1==a1,y1==b1) {
+            (true,true,true,true) => unreachable!("if all side are equal rectangle is not subset"),
+            (false,true,true,true) => self.subtract_as_overlap_type(other,BiggerRight).into(),
+            (true,false,true,true) => self.subtract_as_overlap_type(other,BiggerBottom).into(),
+            (true,true,false,true) => self.subtract_as_overlap_type(other,BiggerLeft).into(),
+            (true,true,true,false) => self.subtract_as_overlap_type(other,BiggerTop).into(),
+            (true,true,false,false) => self.subtract_as_overlap_type(other,TopLeft).into(),
+            (false,true,true,false) => self.subtract_as_overlap_type(other,TopRight).into(),
+            (false,false,true,true) => self.subtract_as_overlap_type(other,BottomRight).into(),
+            (true,false,false,true) => self.subtract_as_overlap_type(other,BottomLeft).into(),
+            (true,false,true,false) => self.subtract_as_overlap_type(other,Horizontal).into(),
+            (false,true,false,true) => self.subtract_as_overlap_type(other,Vertical).into(),
+            (true,false,false,false) => self.subtract_as_overlap_type(other,SmallerLeft).into(),
+            (false,true,false,false) => self.subtract_as_overlap_type(other,SmallerTop).into(),
+            (false,false,true,false) => self.subtract_as_overlap_type(other,SmallerRight).into(),
+            (false,false,false,true) => self.subtract_as_overlap_type(other,SmallerBottom).into(),
+            (false,false,false,false) => Four(
+                Self::new(h.0,v.0,h.2,v.1),
+                Self::new(h.0,v.1,h.1,v.3),
+                Self::new(h.1,v.2,h.3,v.3),
+                Self::new(h.2,v.0,h.3,v.2)
+            )
+        }
+    }
+
+    fn subtract_overlapping(&self,other: &Self) -> SubtractOverlappingRectanglesRes {
+        self.subtract_as_overlap_type(other,self.type_overlap(other))
     }
 
     fn intersect_overlapping(&self,other: &Self) -> IntersectOverlappingRectanglesRes {
@@ -242,7 +270,9 @@ impl AtomOperations for  Rectangle {
             TopLeft => One(Self::new(x0,y0,a1,b1)),
             TopRight => One(Self::new(x0,b0,a1,y1)),
             BottomRight => One(Self::new(a0,b0,x1,y1)),
-            BottomLeft => One(Self::new(a0,y0,x1,b1))
+            BottomLeft => One(Self::new(a0,y0,x1,b1)),
+            Vertical => One(Self::new(a0,y0,a1,y1)),
+            Horizontal => One(Self::new(x0,b0,x1,b1))
         }
     }
 }
@@ -378,9 +408,107 @@ mod tests {
     #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),TopRight,Rectangle::new(41.41,13.11,200.0,67.0);"top right")]
     #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),BottomLeft,Rectangle::new(0.1,25.11,26.0,670.0);"bottom left")]
     #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),BottomRight,Rectangle::new(5.1,67.0,200.0,670.0);"bottom right")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Vertical,Rectangle::new(5.1,17.0,23.0,670.0);"vertical")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Horizontal,Rectangle::new(0.1,22.0,200.0,67.0);"horizontal")]
     fn type_overlap(a: Rectangle, ovrt: RectOverlapping, b: Rectangle) {
         assert_eq!(a.type_overlap(&b),ovrt);
     }
+
+    use CompoundAtomOperationRes::*;
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(3.0,29.89,49.0,79.0),
+    Four(
+        Rectangle::new(1.0,20.0,49.0,29.89),
+        Rectangle::new(1.0,29.89,3.0,80.0),
+        Rectangle::new(3.0,79.0,50.0,80.0),
+        Rectangle::new(49.0,20.0,50.0,79.0)
+    );"common")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(3.0,20.0,49.0,79.0),
+    Three(
+        Rectangle::new(49.0,20.0,50.0,80.0),
+        Rectangle::new(1.0,79.0,49.0,80.0),
+        Rectangle::new(1.0,20.0,3.0,79.0)
+    );"touch up")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(3.0,22.22,49.0,80.0),
+    Three(
+        Rectangle::new(1.0,20.0,3.0,80.0),
+        Rectangle::new(3.0,20.0,50.0,22.22),
+        Rectangle::new(49.0,22.22,50.0,80.0)
+    );"touch down")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(2.0,22.0,50.0,70.0),
+    Three(
+        Rectangle::new(1.0,70.0,50.0,80.0),
+        Rectangle::new(1.0,20.0,2.0,70.0),
+        Rectangle::new(2.0,20.0,50.0,22.0)
+    );"touch right")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,22.0,2.0,70.0),
+    Three(
+        Rectangle::new(1.0,20.0,50.0,22.0),
+        Rectangle::new(2.0,22.0,50.0,80.0),
+        Rectangle::new(1.0,70.0,2.0,80.0)
+    );"touch left")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,20.0,40.0,70.0),
+    Two(
+        Rectangle::new(40.0,20.0,50.0,80.0),
+        Rectangle::new(1.0,70.0,40.0,80.0)
+    );"same top left corner")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(10.0,20.0,50.0,60.0),
+    Two(
+        Rectangle::new(1.0,60.0,50.0,80.0),
+        Rectangle::new(1.0,20.0,10.0,60.0)
+    );"same top right corner")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(10.0,25.5,50.0,80.0),
+    Two(
+        Rectangle::new(1.0,20.0,10.0,80.0),
+        Rectangle::new(10.0,20.0,50.0,25.5)
+    );"same bottom right corner")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,25.2,40.0,80.0),
+    Two(
+        Rectangle::new(1.0,20.0,50.0,25.2),
+        Rectangle::new(40.0,25.2,50.0,80.0)
+    );"same bottom left corner")]
+
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(11.0,20.0,30.0,80.0),
+    Two(
+        Rectangle::new(1.0,20.0,11.0,80.0),
+        Rectangle::new(30.0,20.0,50.0,80.0)
+    );"vertical crossing")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,25.0,50.0,77.0),
+    Two(
+        Rectangle::new(1.0,20.0,50.0,25.0),
+        Rectangle::new(1.0,77.0,50.0,80.0)
+    );"horizontal crossing")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,20.0,50.0,40.0),
+    One(Rectangle::new(1.0,40.0,50.0,80.0));"same top corners")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,23.0,50.0,80.0),
+    One(Rectangle::new(1.0,20.0,50.0,23.0));"same bottom corners")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(1.0,20.0,33.0,80.0),
+    One(Rectangle::new(33.0,20.0,50.0,80.0));"same left corners")]
+    #[test_case(Rectangle::new(1.0,20.0,50.0,80.0),Rectangle::new(11.0,20.0,50.0,80.0),
+    One(Rectangle::new(1.0,20.0,11.0,80.0));"same right corners")]
+    fn subtract_subset(a: Rectangle, b: Rectangle, exp: CompoundAtomOperationRes<Rectangle>){
+        match (a.subtract_subset(&b).into(),exp) {
+            (Four(ra,rb,rc,rd),Four(ea,eb,ec,ed)) => {
+                assert_eq!(ra.as_tuple(),ea.as_tuple());
+                assert_eq!(rb.as_tuple(),eb.as_tuple());
+                assert_eq!(rc.as_tuple(),ec.as_tuple());
+                assert_eq!(rd.as_tuple(),ed.as_tuple());
+            },
+            (Three(ra,rb,rc),Three(ea,eb,ec)) => {
+                assert_eq!(ra.as_tuple(),ea.as_tuple());
+                assert_eq!(rb.as_tuple(),eb.as_tuple());
+                assert_eq!(rc.as_tuple(),ec.as_tuple());
+            },
+            (Two(ra,rb),Two(ea,eb)) => {
+                assert_eq!(ra.as_tuple(),ea.as_tuple());
+                assert_eq!(rb.as_tuple(),eb.as_tuple());
+            },
+            (One(r),One(e)) => assert_eq!(r.as_tuple(),e.as_tuple()),
+            _ => panic!("Result doesn't have the expected shape")
+        }
+    }
+
+
+
 
 
 }
