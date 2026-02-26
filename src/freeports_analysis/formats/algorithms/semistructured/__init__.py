7385c69851405d3645a54f1852275eb324551641
Giuliano Verrando <giulianoverrando@gmail.com>
2026-02-27 00:58:28 +0100

    fixed semistructured get_formats_mapping

--- src/freeports_analysis/formats/algorithms/semistructured/__init__.py
+++ src/freeports_analysis/formats/algorithms/semistructured/__init__.py
@@ -15,7 +15,12 @@ import pandas as pd
 import freeports_analysis.formats.algorithms.semistructured.pdf_filter as p
 import freeports_analysis.formats.algorithms.semistructured.text_extract as t
 import freeports_analysis.formats.algorithms.semistructured.deserialize as d
-from ..commons import index_format_pipe, create_index_format_name_pipe
+from ..commons import (
+    index_format_pipe,
+    create_index_format_name_pipe,
+    FKRelation,
+    column_id_format_pipe,
+)
 
 data = Path(__file__).parent
 
@@ -63,6 +68,7 @@ algorithms_with_deserialize_args = yaml.safe_load(
 
 formats_mapping_schema = pa.DataFrameSchema(
     {
+        "ID": column_id_format_pipe(FKRelation.ONE_TO_ONE),
         "pdf_filter": pa.Column(
             pd.StringDtype,
             [pa.Check(lambda x: x.isin(pdf_filter_funcs))],
@@ -92,7 +98,7 @@ formats_mapping_schema = pa.DataFrameSchema(
     },
     strict=True,
     coerce=True,
-    index=index_format_pipe,
+    index=index_format_pipe(),
 )
 
 
@@ -110,7 +116,7 @@ def get_formats_mapping() -> pd.DataFrame:
     functions should be used for each format and pipeline combination.
     """
     df = pd.read_csv(data / "formats_mapping.csv")
-    df = create_index_format_name_pipe(df)
+    df = create_index_format_name_pipe(df, "", FKRelation.ONE_TO_ONE)
 
     def _input_from_func(x: pd.Series) -> pd.Series:
         """Generate input class names from function names.
@@ -134,103 +140,104 @@ def get_formats_mapping() -> pd.DataFrame:
         InputTextExtract=lambda x: _input_from_func(x["text_extract"]),
         InputDeserialize=lambda x: _input_from_func(x["deserialize"]),
     )
-    # return formats_mapping_schema.validate(df)
-    return df
+    return formats_mapping_schema.validate(df)
 
 
-VALID_ALGORITHM_ID = (
-    get_formats_mapping().index.get_level_values("Computed ID").to_list()
-)
+# VALID_ALGORITHM_ID = get_formats_mapping().index.get_level_values("Computed ID").to_list()
 
 
-def _get_segment(
-    format_name: str, segment_name: str, pipes_mapping: List[Tuple[str, pd.Series]]
-) -> Dict[str, List[Callable]]:
-    """Get processing segment functions for a given format and segment type.
+# def _get_segment(
+#     format_name: str, segment_name: str, pipes_mapping: List[Tuple[str, pd.Series]]
+# ) -> Dict[str, List[Callable]]:
+#     """Get processing segment functions for a given format and segment type.
 
-    Parameters
-    ----------
-    format_name : str
-        Name of the format to process
-    segment_name : str
-        Type of segment ('pdf_filter', 'text_extract', or 'deserialize')
-    pipes_mapping : List[Tuple[str, pd.Series]]
-        List of pipeline mappings with pipeline names and corresponding series
+#     Parameters
+#     ----------
+#     format_name : str
+#         Name of the format to process
+#     segment_name : str
+#         Type of segment ('pdf_filter', 'text_extract', or 'deserialize')
+#     pipes_mapping : List[Tuple[str, pd.Series]]
+#         List of pipeline mappings with pipeline names and corresponding series
 
-    Returns
-    -------
-    Dict[str, List[Callable]]
-        Dictionary mapping pipeline names to lists of processing functions
+#     Returns
+#     -------
+#     Dict[str, List[Callable]]
+#         Dictionary mapping pipeline names to lists of processing functions
 
-    Raises
-    ------
-    KeyError
-        If algorithm configuration is not found for the given format and pipeline
-    """
-    segment: Dict[str, List[Callable]] = {}
-    input_segment = "Input" + segment_name.title().replace("_", "")
-    args = yaml.safe_load((data / segment_name / "args.yaml").open("r"))
+#     Raises
+#     ------
+#     KeyError
+#         If algorithm configuration is not found for the given format and pipeline
+#     """
+#     segment: Dict[str, List[Callable]] = {}
+#     input_segment = "Input" + segment_name.title().replace("_", "")
+#     args = yaml.safe_load((data / segment_name / "args.yaml").open("r"))
 
-    for pipeline, mapping in pipes_mapping:
-        algorithm_id = f"{format_name}({pipeline})"
-        if pd.isna(mapping[segment_name]):
-            continue
-        if pipeline not in segment:
-            segment[pipeline] = []
-        selected_args = None
-        try:
-            selected_args = args[algorithm_id]
-        except KeyError:
-            if pipeline == "":
-                selected_args = args[format_name]
-            else:
-                raise
-        arg = (
-            selected_args[len(segment[pipeline])]
-            if isinstance(selected_args, list)
-            else selected_args
-        )
-        selected_func = getattr(p, mapping[segment_name])
-        selected_input = getattr(p, mapping[input_segment])
-        func = selected_func(selected_input(**arg))(lambda xml_root: None)
-        segment[pipeline].append(func)
-    return segment
+#     for pipeline, mapping in pipes_mapping:
+#         algorithm_id = f"{format_name}({pipeline})"
+#         if pd.isna(mapping[segment_name]):
+#             continue
+#         if pipeline not in segment:
+#             segment[pipeline] = []
+#         selected_args = None
+#         try:
+#             selected_args = args[algorithm_id]
+#         except KeyError:
+#             if pipeline == "":
+#                 selected_args = args[format_name]
+#             else:
+#                 raise
+#         arg = (
+#             selected_args[len(segment[pipeline])]
+#             if isinstance(selected_args, list)
+#             else selected_args
+#         )
+#         selected_func = getattr(p, mapping[segment_name])
+#         selected_input = getattr(p, mapping[input_segment])
+#         func = selected_func(selected_input(**arg))(lambda xml_root: None)
+#         segment[pipeline].append(func)
+#     return segment
 
 
-def get_pipes(
-    format_name: str,
-) -> Tuple[
-    Dict[str, List[Callable]], Dict[str, List[Callable]], Dict[str, List[Callable]]
-]:
-    """Get processing pipelines for a specific format.
+# def get_pipes(
+#     format_name: str,
+# ) -> Tuple[
+#     Dict[str, List[Callable]], Dict[str, List[Callable]], Dict[str, List[Callable]]
+# ]:
+#     """Get processing pipelines for a specific format.
 
-    Parameters
-    ----------
-    format_name : str
-        Name of the format to get pipelines for
+#     Parameters
+#     ----------
+#     format_name : str
+#         Name of the format to get pipelines for
 
-    Returns
-    -------
-    Tuple[Dict[str, List[Callable]], Dict[str, List[Callable]], Dict[str, List[Callable]]]
-        Tuple containing three dictionaries for pdf_filter, text_extract, and deserialize segments.
-        Each dictionary maps pipeline names to lists of processing functions.
+#     Returns
+#     -------
+#     Tuple[Dict[str, List[Callable]], Dict[str, List[Callable]], Dict[str, List[Callable]]]
+#         Tuple containing three dictionaries for pdf_filter, text_extract, and deserialize segments.
+#         Each dictionary maps pipeline names to lists of processing functions.
 
-    Notes
-    -----
-    Returns empty dictionaries if the format name is not found in the mapping.
-    """
-    pipes_mapping: List[Tuple[str, pd.Series]] = []
-    try:
-        selected_row = get_formats_mapping().loc[format_name]
-        pipes_mapping = [
-            (idx[0] if not pd.isna(idx[0]) else "", row)
-            for idx, row in selected_row.iterrows()
-        ]
-    except KeyError:
-        pass
+#     Notes
+#     -----
+#     Returns empty dictionaries if the format name is not found in the mapping.
+#     """
+#     pipes_mapping: List[Tuple[str, pd.Series]] = []
+#     try:
+#         selected_row = get_formats_mapping().loc[format_name]
+#         pipes_mapping = [
+#             (idx[0] if not pd.isna(idx[0]) else "", row)
+#             for idx, row in selected_row.iterrows()
+#         ]
+#     except KeyError:
+#         pass
 
-    pdf_filter_segment = _get_segment(format_name, "pdf_filter", pipes_mapping)
-    text_extract_segment = _get_segment(format_name, "text_extract", pipes_mapping)
-    deserialize_segment = _get_segment(format_name, "deserialize", pipes_mapping)
+#     pdf_filter_segment = _get_segment(format_name, "pdf_filter", pipes_mapping)
+#     text_extract_segment = _get_segment(format_name, "text_extract", pipes_mapping)
+#     deserialize_segment = _get_segment(format_name, "deserialize", pipes_mapping)
 
-    return pdf_filter_segment, text_extract_segment, deserialize_segment
+#     return pdf_filter_segment, text_extract_segment, deserialize_segment
+
+
+def get_pipes():
+    pass
