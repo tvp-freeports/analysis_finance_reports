3655baffcdd19ab421d5c37e40d043db2df7c4e5
Oreste Sciacqualegni <oreste@archLaptopP1Gen6.localdomain>
2026-04-01 17:21:31 +0200

    mediolanum es 24 a managment compnay

--- src/freeports_analysis/formats/algorithms/unstructured/mediolanum_es24_a.py
+++ src/freeports_analysis/formats/algorithms/unstructured/mediolanum_es24_a.py
@@ -4,10 +4,17 @@ from lxml import etree
 from freeports_analysis.formats.utils.pdf_extract import (
     PdfExtractInvestmentsStandard,
     PdfExtractPageClassifyStandard,
+    PdfExtractCurrencyStandard,
+    PdfExtractFundStandard,
+    PdfExtractManagmentCompanyStandard,
 )
-from freeports_analysis.formats.utils.text_filter import TextFilterPageClassifyStandard
+from freeports_analysis.formats.utils.text_filter import (
+    TextFilterPageClassifyStandard,
+    TextFilterManagmentCompanyStandard,
+)
 from freeports_analysis.formats.utils.deserialize import (
     DeserializerPageClassifyStandard,
+    DeserializerManagmentCompanyStandard,
 )
 from freeports_analysis.formats.utils.pdf_extract.pdf_parts import PdfLineSelection
 from freeports_analysis.formats.algorithms.commons import Pipeline
@@ -17,19 +24,49 @@ curr_set = PdfLineSelection(font_size=(8.9, 9.1), text="(expresado en") & h_font
 
 pipelines = {
     "": Pipeline(
-        pdf_extract=PdfExtractPageClassifyStandard(
-            header_sets=[PdfLineSelection.text("Descripci") & h_font, curr_set],
-            page_type="investments",
+        pdf_extract=(
+            PdfExtractPageClassifyStandard(
+                header_sets=[PdfLineSelection.text("Descripci") & h_font, curr_set],
+                page_type="investments",
+            ),
+            PdfExtractPageClassifyStandard(
+                header_sets=[
+                    PdfLineSelection.text(
+                        "Gestor de Inversiones y Gestora de Tesorería"
+                    ),
+                    PdfLineSelection.text(
+                        "Presidente del Consejo de Administración de la Sociedad"
+                    ),
+                    PdfLineSelection.text("Gestores Delegados de Inversiones"),
+                ],
+                page_type="manco",
+            ),
         ),
         text_filter=TextFilterPageClassifyStandard(),
         deserialize=DeserializerPageClassifyStandard(),
     ),
     "investments": Pipeline(
-        pdf_extract=PdfExtractInvestmentsStandard(
-            subfund_set=PdfLineSelection.area(0.0, 58.0, 1e6, 82.0) & h_font,
-            body_set=PdfLineSelection.area(0.0, 0.0, 1e6, 767.0)
-            & h_font / PdfLineSelection.text("^ "),
-            currency_set=curr_set,
+        pdf_extract=(
+            PdfExtractInvestmentsStandard(
+                body_set=PdfLineSelection.area(0.0, 0.0, 1e6, 767.0)
+                & h_font / PdfLineSelection.text("^ ")
+            ),
+            PdfExtractFundStandard(
+                PdfLineSelection.area(0.0, 58.0, 1e6, 82.0) & h_font
+            ),
+            PdfExtractCurrencyStandard(curr_set),
         )
+    ),
+    "manco": Pipeline(
+        pdf_extract=PdfExtractManagmentCompanyStandard(
+            PdfLineSelection.area_from_movewindow(
+                PdfLineSelection.text("Gestor de Inversiones y Gestora de Tesorería"),
+                (-0.1, 0.8),
+                2.0,
+                1.4,
+            )
+        ),
+        text_filter=TextFilterManagmentCompanyStandard(),
+        deserialize=DeserializerManagmentCompanyStandard(),
     ),
 }
