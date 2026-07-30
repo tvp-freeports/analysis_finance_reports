f989e6cd16494d8ffac8df0f589da5ccbbd52bfb
Oreste Sciacqualegni <oreste@archLaptopP1Gen6.localdomain>
2026-07-30 12:51:29 +0200

    commented benchmarks

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -10,21 +10,21 @@ from freeports_analysis.formats.utils.pdf_extract.pdf_parts import (
 from freeports_analysis.formats.utils.pdf_extract.pdf_parts import (
     pdfline_selection_from_str,
 )
-from freeports_analysis.data import get_target_companies
+# from freeports_analysis.data import get_target_companies
 
-pdf_extract = get_pipelines("CARNE-EN23")[""].pdf_extract
 
-root_dir = Path(__file__).parent
-pdf_file = pypdf.Document(root_dir / "report.pdf")
-page_doc = pdf_file[25]
-root_dict = page_doc.get_text("dict")
-lines = pdflines_from_pagedict(root_dict)
-body_blks = pdfline_selection_from_str("ArialMT[6.96](160:786)").select(lines)
+# root_dir = Path(__file__).parent
+# pdf_file = pypdf.Document(root_dir / "report.pdf")
+# page_doc = pdf_file[25]
+# root_dict = page_doc.get_text("dict")
+# lines = pdflines_from_pagedict(root_dict)
+# body_blks = pdfline_selection_from_str("ArialMT[6.96](160:786)").select(lines)
 
-df_target_companies = get_target_companies("TEST")
-target_companies = (
-    freeports_lib.text_filter.matcher.CompanyMatchInfos.compile_from_pandas_df(
-        df_target_companies
-    )
-)
-pdf_blks = pdf_extract(root_dict)
+# df_target_companies = get_target_companies("TEST")
+
+# target_companies = (
+#     freeports_lib.text_filter.matcher.CompanyMatchInfos.compile_from_pandas_df(
+#         df_target_companies
+#     )
+# )
+# pdf_blks = pdf_extract(root_dict)
--- benchmarks/test_text_extract.py
+++ benchmarks/test_text_extract.py
@@ -1,19 +1,19 @@
-import freeports_lib
-from freeports_analysis.formats.algorithms import get_pipelines
-from .conftest import pdf_blks, target_companies
-import pytest
+# import freeports_lib
+# from freeports_analysis.formats.algorithms import get_pipelines
+# from .conftest import pdf_blks, target_companies
+# import pytest
 
 
-text_filter = get_pipelines("CARNE-EN23")[""].text_filter
+# text_filter = get_pipelines("CARNE-EN23")[""].text_filter
 
 
-@pytest.mark.benchmarks
-def test_text_filter(benchmark):
-    result = benchmark(text_filter, pdf_blks, target_companies)
+# @pytest.mark.benchmarks
+# def test_text_filter(benchmark):
+#     result = benchmark(text_filter, pdf_blks, target_companies)
 
 
-@pytest.mark.benchmarks
-def test_match_company(benchmark):
-    result = benchmark(
-        freeports_lib.text_filter.matcher.match_company, "fjdlsajfals", target_companies
-    )
+# @pytest.mark.benchmarks
+# def test_match_company(benchmark):
+#     result = benchmark(
+#         freeports_lib.text_filter.matcher.match_company, "fjdlsajfals", target_companies
+#     )
